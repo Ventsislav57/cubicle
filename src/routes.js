bde564848901bd4d8b0afa80200d94b44e61b45f
Ventsislav57 <106383945+Ventsislav57@users.noreply.github.com>
2022-05-27 17:16:37 +0300

    Add create and about page

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const homeControllers = require('./controllers/homeControllers');
+const cubeControllers = require('./controllers/cubeControllers');
 
 const router = express.Router();
 
 router.get('/', homeControllers.index);
+router.get('/about', homeControllers.about)
+router.use('/cube', cubeControllers)
 
 module.exports = router
