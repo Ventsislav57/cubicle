15d8222a0e97565a1a7aad9113b26e2117cbe74e
Ventsislav57 <106383945+Ventsislav57@users.noreply.github.com>
2022-05-27 15:48:57 +0300

    Render home page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 const port = 5000;
 
 app.use('/static', express.static('public'));
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.render('index');
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}...`));
