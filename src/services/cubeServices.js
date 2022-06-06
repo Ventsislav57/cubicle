0cd64ca6721fc64b1b7299ccd613c95c3b353516
Ventsislav57 <106383945+Ventsislav57@users.noreply.github.com>
2022-06-06 12:03:12 +0300

    Create dataBase

--- src/services/cubeServices.js
+++ src/services/cubeServices.js
@@ -6,7 +6,7 @@ const cubes = require('../db.json');
 exports.getOne = (cubeId) => cubes[cubeId];
 
 exports.save = (cube) => {
-    cubes.push(cube);
+    cubes.push({id: cubes[cubes.length - 1].id + 1, ...cubes});
 
     let textData = JSON.stringify(cubes, '', 2);
     return fs.writeFile(path.resolve('src', 'db.json'), textData, { encoding: 'utf-8' });
