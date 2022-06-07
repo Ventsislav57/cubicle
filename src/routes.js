d59818b456b479d5a7d156df8da2c7174435ad52
Ventsislav57 <106383945+Ventsislav57@users.noreply.github.com>
2022-06-07 22:04:14 +0300

    Add create accessory page

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const homeControllers = require('./controllers/homeControllers');
 const cubeControllers = require('./controllers/cubeControllers');
+const accessoryControllers = require('./controllers/accessoryControllers');
+
 
 const router = express.Router();
 
 router.use('/', homeControllers);
-router.use('/cube', cubeControllers)
+router.use('/cube', cubeControllers);
+router.use('/accessory', accessoryControllers);
+
 
 module.exports = router;
